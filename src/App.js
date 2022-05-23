490352bd2eae57e6d0f80592482fcf72c4ff81c4
awsmPuff <itsanthea915@gmail.com>
2022-05-23 04:30:04 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import sounds from './sounds';
 
 function App() {
+ 
+  const [key, setKey] = useState("");
+
+  useEffect(() => {
+    document.addEventListener('keydown', (e) => {
+      handleClick(e.key.toUpperCase())
+    });
+  })
+
+  function handleClick(drum) {
+    const audio = document.getElementById(drum);
+     audio.play();   
+     setKey(drum);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drum-machine">
+    <h3>PLAY THE DRUM</h3>
+    <div id="display">{key}</div>
+    <div className='drum-pads'>
+        {sounds.map(x => (
+          <div 
+          key={x.src}
+          className='drum-pad' 
+          id={x.src} 
+          onClick={() => {
+          handleClick(x.letter)
+        }}>
+        {x.letter}
+        <audio id = {x.letter} className='clip' src={x.src}></audio>
+        </div>)
+        )}
+    </div>
     </div>
   );
 }
---
+++ src/sounds.js
@@ -0,0 +1,50 @@
+const sounds = [
+    {
+        keyCode:81,
+        letter:"Q",
+        src:"https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+    },
+    {
+        keyCode:87,
+        letter:"W",
+        src:"https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+    },
+    {
+        keyCode:69,
+        letter:"E",
+        src:"https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+    },
+    {
+        keyCode:65,
+        letter:"A",
+        src:"https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+    },
+    {
+        keyCode:83,
+        letter:"S",
+        src:"https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+    },
+    {
+        keyCode:68,
+        letter:"D",
+        src:"https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+    },
+    {
+        keyCode:90,
+        letter:"Z",
+        src:"https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+    },
+    {
+        keyCode:88,
+        letter:"X",
+        src:"https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+    },
+    {
+        keyCode:67,
+        letter:"C",
+        src:"https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+    }
+];
+
+
+export default sounds;
